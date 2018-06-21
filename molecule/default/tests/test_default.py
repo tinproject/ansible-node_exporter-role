149911a14ba75f7cb9df7d312611a8b36f2748c4
Agustín Herranz <agustin.herranz@gmail.com>
2018-06-21 14:02:36 +0200

    Add molecule testing

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,21 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_node_exporter_service(host):
+    service = host.service('node_exporter')
+
+    assert service.is_running
+    assert service.is_enabled
